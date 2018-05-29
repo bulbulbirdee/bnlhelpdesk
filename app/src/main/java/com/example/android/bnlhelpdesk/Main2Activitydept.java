03e32d55de871e07c4fc21c8424ccff55e69a706
Bulbul <bulbulbirdee@gmail.com>
2018-05-29 11:45:24 +0530

    hello

---
+++ app/src/main/java/com/example/android/bnlhelpdesk/Main2Activitydept.java
@@ -0,0 +1,32 @@
+package com.example.android.bnlhelpdesk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activitydept extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2_activitydept);
+
+        final Button button =  findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                //intent.putExtra("score",score);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+}
